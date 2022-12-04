0f202e96928848110be5a9d11d2bcdfaf71bcb5f
Abragus <gustav.a.abrahamsson@gmail.com>
2022-12-04 21:16:29 +0100

    Change network credential description

--- project/src/EXAMPLE_network_credentials.h
+++ project/src/EXAMPLE_network_credentials.h
@@ -1,4 +1,5 @@
-// Supports adding multiple networks. If multiple are available, it will connect to the first one specified here. 
+// Supports adding multiple networks. If multiple are available, 
+// it will try to connect to them in the order they are specified here. 
 #define WIFI_SSID { "my_network_name", "network_name_2" }
 #define WIFI_PASSWORD { "my_password", "password_2" }
 #define WIFI_ID { "", "id_2" } // Mailaddress för skolans WiFi, annars ""
